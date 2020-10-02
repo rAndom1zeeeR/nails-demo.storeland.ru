08c9e223e992d17a41450ce83ecce7c038347f30
Jora <tjorav@gmail.com>
2020-10-02 19:22:39 +0300

    [fix] Адаптивность

--- files/main.js
+++ files/main.js
@@ -557,7 +557,7 @@ function goodspage() {
       0:{items:2},
       320:{items:2},
       480:{items:3},
-      641:{items:3},
+      641:{items:4},
       768:{items:4},
       992:{items:2},
       1200:{items:3},
